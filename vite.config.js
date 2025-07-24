2ded107604c80bec2e7ad78fe7e491565f923e3e
mohammed <midooooahmed28@gmail.com>
2025-07-25 01:25:06 +0300

    تحديث المشروع بعد التعديلات

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/course-/',  // ← ده مهم جدًا
+  base: '/course-/', // <<< غيّر ده ليطابق اسم الريبو بالضبط
   plugins: [react()],
 })
